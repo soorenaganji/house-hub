e63d4b6d3cbe247bfd081061e9f298ef81263440
soorenaganji <109369184+soorenaganji@users.noreply.github.com>
2024-06-02 22:53:59 +0330

    importing the hero section in the landing page

---
+++ app/components/templates/Landing.js
@@ -0,0 +1,11 @@
+import HeroSection from "../modules/HeroSection";
+
+const Landing = () => {
+  return (
+    <div className=" bg-gradient-to-b from-[#E0DEF7] to-transparent px-4 pt-36 min-h-screen ">
+      <HeroSection />
+    </div>
+  );
+};
+
+export default Landing;
