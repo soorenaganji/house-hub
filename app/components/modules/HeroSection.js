7f4c98751470921dfcad17afe49c3ff810a45861
soorenaganji <109369184+soorenaganji@users.noreply.github.com>
2024-06-02 22:53:08 +0330

    writing the Hero section copy

--- app/components/modules/HeroSection.js
+++ app/components/modules/HeroSection.js
@@ -1,6 +1,14 @@
 const HeroSection = () => {
   return (
     <>
+      <h1 className="text-4xl text-center font-bold ">
+        Buy, rent, or sell your property easily
+      </h1>
+      <p className="text-lg mt-8 text-center">
+        A great platform to buy, sell, or even rent your properties without any
+        commisions.
+      </p>
+      </div>
     </>
   );
 };
