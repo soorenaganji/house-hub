fd1b0ab25160aa8e50e3f1e33b42b999f16af302
soorenaganji <109369184+soorenaganji@users.noreply.github.com>
2024-06-08 15:36:38 +0330

    adding the User model

---
+++ app/models/User.js
@@ -0,0 +1,29 @@
+import { Schema, models, model } from "mongoose";
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    immutable: true,
+  },
+});
+
+const User = models.User || model("User", userSchema);
+
+export default User;
