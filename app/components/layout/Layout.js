9b02c9543bffc32274683e28d614426a8068f263
soorenaganji <109369184+soorenaganji@users.noreply.github.com>
2024-06-08 15:44:59 +0330

    adding actual href s to layout and optimizing

--- app/components/layout/Layout.js
+++ app/components/layout/Layout.js
@@ -3,54 +3,93 @@ import Header from "./Header";
 import Link from "next/link";
 import { useState } from "react";
 import Footer from "./Footer";
+import { usePathname } from "next/navigation";
+import { useSession } from "next-auth/react";
 const Layout = ({ children }) => {
+  const { data } = useSession();
   const [isNavBarOpen, setIsNavbarOpen] = useState(false);
+  const pathname = usePathname();
+  const isAuthPage = pathname === "/signup" || pathname === "/login";
+  const isDashboard =
+    pathname === "/dashboard/add-post" ||
+    pathname === "/dashboard" ||
+    pathname === "/dashboard/my-posts" ||
+    pathname === "/dashboard/account" ;
   return (
     <>
-      {" "}
-      <div className="transition-all duration-150 fixed z-50 top-0  w-full  ">
-        <Header isNavBarOpen={isNavBarOpen} setIsNavbarOpen={setIsNavbarOpen} />
+      {!isAuthPage ? (
+        <>
+          <div className="transition-all duration-150 fixed z-50 top-0  w-full">
+            <Header
+              isNavBarOpen={isNavBarOpen}
+              setIsNavbarOpen={setIsNavbarOpen}
+            />
 
-        <div
-          className={` absolute transition-all duration-150 overflow-hidden  right-0  border-l ${
-            isNavBarOpen
-              ? "bg-[#00000072] h-screen w-screen "
-              : "border-none  bg-transparent h-0"
-          }
-        `}
-        >
-          <div
-            className={` absolute bg-white right-0 transition-all duration-150  border-l ${
-              isNavBarOpen
-                ? "h-screen w-[45%]right-0 "
-                : "  border-none bg-transparent -right-80 "
-            } `}
-          >
             <div
-              className={`flex flex-col gap-4 px-4 transition-all duration-150 pt-8 ${
-                isNavBarOpen ? "" : "hidden"
-              } `}
+              className={`absolute transition-all duration-150 overflow-hidden right-0 border-l ${
+                isNavBarOpen
+                  ? "bg-[#00000072] h-screen w-screen"
+                  : "border-none bg-transparent h-0"
+              }`}
             >
-              <Link
-                href={"/"}
-                className="bg-primary text-white w-20 h-10 mx-auto flex items-center justify-center rounded-md "
+              <div
+                className={`absolute bg-white right-0 transition-all duration-150 border-l ${
+                  isNavBarOpen
+                    ? "h-screen w-[45%] right-0"
+                    : "border-none bg-transparent -right-80"
+                }`}
               >
-                Login
-              </Link>
-              <Link href={"/"} className="text-primary mx-auto">
-                Signup
-              </Link>
-              <Link href={"/"}>Rent</Link>
-              <Link href={"/"}>Buy</Link>
-              <Link href={"/"}>Sell</Link>
-              <Link href={"/"}>ManageProperty</Link>
-              <Link href={"/"}>Resources</Link>
+                <div
+                  className={`flex flex-col gap-4 px-4 transition-all duration-150 pt-8 ${
+                    isNavBarOpen ? "" : "hidden"
+                  }`}
+                >
+                  {data ? (
+                    <Link
+                      onClick={() => setIsNavbarOpen(false)}
+                      href={"/dashboard"}
+                    >
+                      Dashboard
+                    </Link>
+                  ) : (
+                    <>
+                      {" "}
+                      <Link
+                        onClick={() => setIsNavbarOpen(false)}
+                        href={"/login"}
+                        className="bg-primary text-white w-20 h-10 mx-auto flex items-center justify-center rounded-md"
+                      >
+                        Login
+                      </Link>
+                      <Link
+                        onClick={() => setIsNavbarOpen(false)}
+                        href={"/signup"}
+                        className="text-primary mx-auto"
+                      >
+                        Signup
+                      </Link>{" "}
+                    </>
+                  )}
+                  <Link onClick={() => setIsNavbarOpen(false)} href={"/rent"}>
+                    Rent
+                  </Link>
+                  <Link onClick={() => setIsNavbarOpen(false)} href={"/buy"}>
+                    Buy
+                  </Link>
+                  <Link onClick={() => setIsNavbarOpen(false)} href={"/sell"}>
+                    Sell
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-      {children}
-      <Footer />
+
+          <main className="min-h-screen pt-20">{children}</main>
+          {!isDashboard && <Footer />}
+        </>
+      ) : (
+        <main className="min-h-screen">{children}</main>
+      )}
     </>
   );
 };
