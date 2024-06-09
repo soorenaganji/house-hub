e795e485f1b298acdca270184fc90f67439ab46e
soorenaganji <109369184+soorenaganji@users.noreply.github.com>
2024-06-09 22:22:09 +0330

    making SearchBox dynamic

--- app/components/modules/SearchBox.js
+++ app/components/modules/SearchBox.js
@@ -1,19 +1,34 @@
-import { LuSearch } from "react-icons/lu";
-import SearchBar from "../elements/SearchBar";
+import { useState } from 'react';
+import { LuSearch } from 'react-icons/lu';
+import SearchBar from '../elements/SearchBar';
+
 const SearchBox = () => {
+  const [selectedField, setSelectedField] = useState('Rent');
+
+  const handleFieldClick = (field) => {
+    setSelectedField(field);
+  };
+
   return (
     <div className="w-full bg-white rounded-lg mt-8">
       <div className="flex items-center justify-between px-8 py-4 border-b">
-        <button>Rent</button>
-        <button>Buy</button>
-        <button>Sell</button>
+        {['Rent', 'Buy', 'Sell'].map((field) => (
+          <button
+            key={field}
+            onClick={() => handleFieldClick(field)}
+            className={`pb-2 ${
+              selectedField === field ? 'border-b-2 border-primary' : ''
+            }`}
+          >
+            {field}
+          </button>
+        ))}
       </div>
       <div className="p-4 flex justify-between items-center">
         <SearchBar
-          styles={
-            "h-10 placeholder:font-thin border-none w-56 outline-none px-2"
-          }
-          placeHolder={"Search Location"}
+          styles="h-10 placeholder:font-thin border-none w-56 outline-none px-2"
+          placeHolder="Search Location"
+          selectedField={selectedField}
         />
         <button className="bg-primary h-12 w-12 rounded-md text-white text-2xl flex items-center justify-center">
           <LuSearch />
