0352aa0c634040d0589d551ac4f6960354a01b5c
soorenaganji <109369184+soorenaganji@users.noreply.github.com>
2024-06-25 01:31:25 +0330

    configuring an extended color pallette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors : {
-        primary : "#7065F0"
-      }
+      colors: {
+        primary: "#7065F0",
+        secondary: "#ff3067",
+        accent: "#3b2a82",
+      },
     },
   },
   plugins: [],
-}
+};
