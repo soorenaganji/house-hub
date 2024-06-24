801f9cdd1346351bf82ee712df7b07a25cab2836
soorenaganji <109369184+soorenaganji@users.noreply.github.com>
2024-06-25 01:43:10 +0330

    improving the landing UI

--- app/components/templates/Landing.js
+++ app/components/templates/Landing.js
@@ -8,7 +8,7 @@ import TestimonialsSection from "../modules/TestimonialsSection";
 const Landing = () => {
   return (
     <div className="flex flex-col gap-16 ">
-      <div className=" bg-gradient-to-b from-[#E0DEF7] to-transparent pt-36 px-4  ">
+      <div className=" bg-gradient-to-b from-[#e0defb] via-teal-50 via-30% to-transparent pt-36 px-4  ">
         <HeroSection />
       </div>
       <div className="px-4 flex flex-col gap-16">
