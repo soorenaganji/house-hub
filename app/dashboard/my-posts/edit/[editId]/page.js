9486d6389dea65717c5b09ca76aae3c5d1f461cd
soorenaganji <109369184+soorenaganji@users.noreply.github.com>
2024-06-25 01:36:17 +0330

    adding editPage and getting the data

---
+++ app/dashboard/my-posts/edit/[editId]/page.js
@@ -0,0 +1,63 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import User from "@/app/models/User";
+import connectDB from "@/app/utils/connectDB";
+import EditPost from "@/app/components/templates/EditPost";
+
+const PostEditPage = async ({ params }) => {
+  const { editId } = params; // Extract the postId from the params
+  console.log(editId);
+  async function fetchData(editId) {
+    await connectDB();
+    const session = await getServerSession(authOptions);
+    const user = await User.aggregate([
+      { $match: { email: session.user.email } },
+      {
+        $lookup: {
+          from: "profiles",
+          foreignField: "userId",
+          localField: "_id",
+          as: "posts",
+        },
+      },
+    ]);
+
+    const post = await user[0].posts.find((p) => p._id.valueOf() === editId);
+console.log(post)
+    let formData = {};
+    let images = [];
+    if (post) {
+      formData = {
+        title: post.title,
+        description: post.description,
+        city: post.city,
+        street: post.street,
+        zipcode: post.zipcode,
+        size: post.size,
+        bedroomsCount: post.bedroomsCount,
+        bathroomsCount: post.bathroomsCount,
+        phoneNumber: post.phoneNumber,
+        email: post.email,
+        rentalOrSell: post.rentalOrSell,
+        deposit: post.deposit,
+        mortgage: post.mortgage,
+        price: post.price,
+        facilities: post.facilities,
+        rules: post.rules,
+      };
+      images = post.imageUrls?.map((url) => ({ url }));
+    }
+
+    return { formData, images };
+  }
+  const { formData, images } = await fetchData(editId);
+  console.log(formData)
+  return (
+    <>
+      {" "}
+      <EditPost data={formData} imageUrls={images} id={editId} />
+    </>
+  );
+};
+
+export default PostEditPage;
