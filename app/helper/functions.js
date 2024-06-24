d17ce2f5fbf085633325dee6f589ddaf2295fbe3
soorenaganji <109369184+soorenaganji@users.noreply.github.com>
2024-06-25 01:34:57 +0330

    developing formatNumber function

--- app/helper/functions.js
+++ app/helper/functions.js
@@ -1,9 +1,11 @@
 import supabase from "lib/supabaseClient";
 function formatNumber(num) {
-    if (num >= 1000) {
-        return (num / 1000).toFixed(0) + 'K';
-    }
-    return num.toString();
+  if (num >= 1000000) {
+      return (num / 1000000).toFixed(0) + 'M';
+  } else if (num >= 1000) {
+      return (num / 1000).toFixed(0) + 'k';
+  }
+  return num.toString();
 }
 const uploadImageToSupabase = async (file) => {
     const fileExt = file.name.split(".").pop();
