a70c80321fd73cc1693d59ab2f89c16b6249284e
soorenaganji <109369184+soorenaganji@users.noreply.github.com>
2024-06-25 01:42:51 +0330

    adding typewriter animation to the Hero Section

--- app/components/modules/HeroSection.js
+++ app/components/modules/HeroSection.js
@@ -1,17 +1,35 @@
+"use client";
+import { useState } from "react";
+import { Typewriter } from "react-simple-typewriter";
 import SearchBox from "./SearchBox";
 import NumbersBox from "./NumbersBox";
 import number1 from "public/number1.png";
 import number2 from "public/number2.png";
+
 const HeroSection = () => {
+  const words = ["Buy", "Sell", "Rent"];
   return (
     <>
-      <h1 className="text-4xl text-center font-bold ">
-        Buy, rent, or sell your property easily
-      </h1>
-      <p className="text-lg mt-8 text-center">
-        A great platform to buy, sell, or even rent your properties without any
-        commisions.
-      </p>
+      <div className="text-center mt-16">
+        <h1 className="text-5xl leading-snug font-bold mb-4">
+          <span >
+            <Typewriter
+              words={words}
+              loop={0}
+              cursor
+              cursorStyle="_"
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1200}
+            />
+          </span>
+          <span className="mt-2 block">Your Property easily</span>
+        </h1>
+        <p className="text-lg mt-8">
+          A great platform to buy, sell, or even rent your properties without any
+          commissions.
+        </p>
+      </div>
       <SearchBox />
       <div className="flex items-center justify-between mt-16">
         <NumbersBox
