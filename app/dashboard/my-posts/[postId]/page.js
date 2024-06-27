ed3d3d3e82a092cedaf58e8d7d6c4738f8567c85
soorenaganji <109369184+soorenaganji@users.noreply.github.com>
2024-06-27 13:06:48 +0330

    getting post data for preview page

---
+++ app/dashboard/my-posts/[postId]/page.js
@@ -0,0 +1,61 @@
+import PostView from "@/app/components/templates/PostView";
+import connectDB from "@/app/utils/connectDB";
+import User from "@/app/models/User";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+const page = async ({ params }) => {
+  const id = params.postId;
+  console.log(id);
+  console.log(id);
+  async function fetchData(id) {
+    await connectDB();
+    const session = await getServerSession(authOptions);
+    const user = await User.aggregate([
+      { $match: { email: session.user.email } },
+      {
+        $lookup: {
+          from: "profiles",
+          foreignField: "userId",
+          localField: "_id",
+          as: "posts",
+        },
+      },
+    ]);
+
+    const post = await user[0].posts.find((p) => p._id.valueOf() === id);
+    console.log(post);
+    let postsData = {};
+    let images = [];
+    if (post) {
+      postsData = {
+        title: post.title,
+        description: post.description,
+        city: post.city,
+        street: post.street,
+        zipcode: post.zipcode,
+        size: post.size,
+        bedroomsCount: post.bedroomsCount,
+        bathroomsCount: post.bathroomsCount,
+        phoneNumber: post.phoneNumber,
+        email: post.email,
+        rentalOrSell: post.rentalOrSell,
+        deposit: post.deposit,
+        mortgage: post.mortgage,
+        price: post.price,
+        facilities: post.facilities,
+        rules: post.rules,
+      };
+      images = post.imageUrls?.map((url) => ({ url }));
+    }
+
+    return { postsData, images };
+  }
+  const { postsData, images } = await fetchData(id);
+  return (
+    <div>
+      <PostView data={postsData} images={images} />
+    </div>
+  );
+};
+
+export default page;
