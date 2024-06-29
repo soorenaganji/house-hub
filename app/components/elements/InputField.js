3139c1c3c0533c6da2b39a30f461571b78d797ed
soorenaganji <109369184+soorenaganji@users.noreply.github.com>
2024-06-29 23:16:09 +0330

    inputfield UI improvement

--- app/components/elements/InputField.js
+++ app/components/elements/InputField.js
@@ -1,15 +1,14 @@
 const InputField = ({ label, name, type, value, onChange }) => (
-    <div>
-      <label className="block mb-2">{label}</label>
-      <input
-        type={type}
-        name={name}
-        value={value}
-        onChange={onChange}
-        className="w-full border p-2 rounded focus:border-primary focus:shadow-md focus:outline-none transition-all duration-150"
-      />
-    </div>
-  );
-  
-  export default InputField;
-  
+  <div>
+    <label className="block mb-2">{label}</label>
+    <input
+      type={type}
+      name={name}
+      value={value}
+      onChange={onChange}
+      className="w-full border p-2 rounded focus:border-primary mb-4 focus:shadow-md focus:outline-none transition-all duration-150"
+    />
+  </div>
+);
+
+export default InputField;
