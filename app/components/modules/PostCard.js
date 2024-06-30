01910fe515582579fcc3a483a8c41a88e3ec5fcf
soorenaganji <109369184+soorenaganji@users.noreply.github.com>
2024-06-30 22:46:54 +0330

    making posts images fullscreen

--- app/components/modules/PostCard.js
+++ app/components/modules/PostCard.js
@@ -1,12 +1,15 @@
 "use client";
+import { useState } from "react";
 import { BiBath } from "react-icons/bi";
 import { TbSquareRotated } from "react-icons/tb";
-
 import { LiaBedSolid } from "react-icons/lia";
 import Image from "next/image";
 import NoImage from "public/noPhoto.jpeg";
 import { formatNumber } from "@/app/helper/functions";
-import { FaTags , FaKey } from "react-icons/fa";
+import { FaTags, FaKey } from "react-icons/fa";
+import ReactImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
+
 const PostCard = ({
   title,
   description,
@@ -28,6 +31,8 @@ const PostCard = ({
   userId,
   isOnAccountPage,
 }) => {
+  const [isGalleryOpen, setIsGalleryOpen] = useState(false);
+
   const handleImageUrl = () => {
     if (
       imageUrls &&
@@ -40,33 +45,58 @@ const PostCard = ({
       return NoImage;
     }
   };
+
+  const galleryImages =
+    imageUrls && imageUrls.length
+      ? imageUrls.map((url) => ({
+          original: url,
+          thumbnail: url,
+        }))
+      : [{ original: NoImage, thumbnail: NoImage }];
+
+  const handleImageClick = () => {
+    setIsGalleryOpen(true);
+  };
+
+  const handleGalleryClose = () => {
+    setIsGalleryOpen(false);
+  };
+
+  const renderItem = (item) => (
+    <img
+      src={item.original}
+      alt="Gallery image"
+      onError={(e) => {
+        e.target.onerror = null; // Prevent looping
+        e.target.src = NoImage.src;
+      }}
+      className="w-72 h-72 object-cover mx-auto mt-36 "
+    />
+  );
+
   return (
     <>
       <div className="">
-        <div
-          className={` ${
-            isOnAccountPage ? "hidden" : ""
-          }  relative w-24  top-8 -mt-2`}
-        >
+        <div className={`  relative w-24 top-8 -mt-2`}>
           <div
             className={`bg-white/80 backdrop-blur-lg ${
               price ? "text-primary" : "text-secondary"
-            }  px-3 py-1 text-xs rounded-r-lg flex items-center justify-center gap-2`}
+            } px-3 py-1 text-xs rounded-r-lg flex items-center justify-center gap-2`}
           >
             {price ? <FaKey /> : <FaTags />}
             {price ? "For Sale" : "For Rent"}
           </div>
         </div>
 
-        <Image
-          src={handleImageUrl()}
-          alt={title}
-          className={`w-[400px] h-48 object-cover rounded-t-lg mb-1 ${
-            isOnAccountPage ? "hidden" : ""
-          } `}
-          width={400}
-          height={300}
-        />
+        <div onClick={handleImageClick}>
+          <Image
+            src={handleImageUrl()}
+            alt={title}
+            className={`w-[400px] h-48 object-cover rounded-t-lg mb-1 `}
+            width={400}
+            height={300}
+          />
+        </div>
       </div>
       <div className="p-4">
         <div className="text-primary font-extrabold text-lg flex items-center justify-start">
@@ -88,9 +118,7 @@ const PostCard = ({
         <div className="font-bold text-xl my-2">{title}</div>
         <div className="text-gray-500 mb-3">{city}</div>
         <div
-          className={`flex items-center text-gray-700 text-sm pb-2 border-b ${
-            isOnAccountPage ? "hidden" : ""
-          } `}
+          className={`flex items-center text-gray-700 text-sm pb-2 border-b `}
         >
           <LiaBedSolid
             className={`mr-2 ${price ? "text-primary" : "text-secondary"}`}
@@ -106,6 +134,27 @@ const PostCard = ({
           {size} m²
         </div>
       </div>
+
+      {isGalleryOpen && (
+        <div className="fixed inset-0 z-50 bg-black bg-opacity-75 flex items-center justify-center">
+          <div className="relative w-full h-full">
+            <button
+              className="absolute top-4 right-4 text-white text-2xl z-50"
+              onClick={handleGalleryClose}
+            >
+              &times;
+            </button>
+            <ReactImageGallery
+              items={galleryImages}
+              showThumbnails={false}
+              showFullscreenButton={false}
+              showPlayButton={false}
+              showIndex={true}
+              renderItem={renderItem}
+            />
+          </div>
+        </div>
+      )}
     </>
   );
 };
