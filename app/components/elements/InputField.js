70e2e07dce61fa1f87ef96b0a085f6f6c759ed0e
soorenaganji <109369184+soorenaganji@users.noreply.github.com>
2024-07-03 22:38:18 +0330

    email input UI improvement

--- app/components/elements/InputField.js
+++ app/components/elements/InputField.js
@@ -8,7 +8,7 @@ const InputField = ({ label, name, type, value, onChange, readOnly }) => (
         name={name}
         value={value}
         onChange={onChange}
-        className="w-full border p-2 rounded focus:border-primary mb-4 focus:shadow-md focus:outline-none transition-all duration-150"
+        className="w-full border p-2 rounded bg-slate-400 text-slate-800 cursor-not-allowed mb-4 focus:shadow-md focus:outline-none transition-all duration-150"
       />
     ) : (
       <input
